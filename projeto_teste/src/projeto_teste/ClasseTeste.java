95cbbfe806d28a404244464352924a2418ed163c
Alexandre Kranz <ahkranz79@gmail.com>
2024-04-28 17:57:56 -0300

    Primeiro commit

---
+++ projeto_teste/src/projeto_teste/ClasseTeste.java
@@ -0,0 +1,14 @@
+package projeto_teste;
+
+public class ClasseTeste {
+
+	public ClasseTeste() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
